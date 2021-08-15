4b921b97ca9562ddefc47af701414681f4edab16
Professor Roberto <professorroberto.luizrosa@gmail.com>
2021-08-15 20:39:36 -0300

    acerto no namespace

--- CalculadoraDeMultiplicar/frmMultiplicar.cs
+++ CalculadoraDeMultiplicar/frmMultiplicar.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
-namespace CalculadoraDeSomar
+namespace CalculadoraDeMultiplicar
 {
     public partial class frmMultiplicar : Form
     {
